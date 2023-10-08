74089349be30a5c94c4f8fffdeef0403e7f86968
0xR3TR0 <oliwier@klefas.com>
2023-10-08 22:06:31 +0200

    Stable version

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ var icon = document.getElementById("icon");
 
 icon.onclick = function(){
     document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        icon.src = "Images/sun.png";
+    }
+    else{
+        icon.src = "Images/moon.png";
+    }
 }
